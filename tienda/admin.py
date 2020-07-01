c36072147907b8982303f64895d22d02ff216b5f
Thiago <thiagonicomb.xd@gmail.com>
2020-07-01 20:51:05 -0300

    ejercicio 3

--- tienda/admin.py
+++ tienda/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from tienda.models import Comuna
 from tienda.models import *
 
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['RUT_C', 'Nombre', 'Telefono']
 
 # Register your models here.
 
 admin.site.register(Comuna, )
 admin.site.register(Ciudad, )
-admin.site.register(Cliente, )
+admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Categoria, )
 admin.site.register(Proveedor, )
 admin.site.register(Producto, )
