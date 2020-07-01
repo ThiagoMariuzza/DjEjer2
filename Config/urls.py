5bfaa67196ad6f8044708e3cb01a44d300e12c87
Thiago <thiagonicomb.xd@gmail.com>
2020-07-01 20:39:14 -0300

    agregar Baton Admin interface

--- Config/urls.py
+++ Config/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+#from django.contrib import admin
+from django.urls import path, include
+from baton.autodiscover import admin
 
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#]
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('baton/', include('baton.urls')),
 ]
